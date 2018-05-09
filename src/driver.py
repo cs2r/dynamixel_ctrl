bb429b6b6fc0a1ac5f0caa5ad9267e486bb5a09a
cs2r <ceneterofroboticsg108@gmail.com>
2018-05-09 13:35:13 +0000

    topics same as for gazebo

--- src/driver.py
+++ src/driver.py
@@ -7,6 +7,7 @@ from std_msgs.msg import Float64
 
 class arm_driver():
     def __init__(self):
+        self.joint_name = ["shoulder", "elbow", "wrist_pitch", "wrist_rot", "gripper"]
         self.motor = dict()
         self.motor_pub = dict()
         self.cmd = dict()
@@ -15,7 +16,7 @@ class arm_driver():
         for i in range(1, 6):
             rospy.Subscriber("/m" + str(i) + "_controller/state", JointState, self.get_motor_stat, callback_args=i)
             self.motor_pub[str(i)] = rospy.Publisher("/m" + str(i) + "_controller/command", Float64, queue_size=1)
-            rospy.Subscriber("cmd_" + str(i), Float64, self.set_cmd, callback_args=i)
+            rospy.Subscriber("hek/" + self.joint_name[i-1] + "_joint_position_controller/command", Float64, self.set_cmd, callback_args=i)
             self.angle[str(i)] = rospy.Publisher("/motor" + str(i) + "_angle", Float64, queue_size=1)
 
 
@@ -39,15 +40,16 @@ class arm_driver():
             self.angle[str(i)].publish(self.motor[i]["current_pos"])
 
     def set_cmd(self, data, i):
+        command = math.degrees(data.data)
         if (i == 4):
-            self.joint_roll = self.angle_cmd(i, data.data)
+            self.joint_roll = self.angle_cmd(i, command)
 
         else:
-            self.motor_pub[str(i)].publish(math.radians(self.angle_cmd(i, data.data)))
+            self.motor_pub[str(i)].publish(math.radians(self.angle_cmd(i, command)))
 
     def angle_cmd(self, joint, cmd):
         if joint == 1:
-            angle = 35.0/18.0*cmd+45.0/2.0
+            angle = 35.0/18.0*(90-cmd)+45.0/2.0
             return angle
         elif joint == 2:
             angle = 89.0/90.0*cmd+179.0
@@ -59,7 +61,7 @@ class arm_driver():
             angle = -3.0/2.0*cmd+270.0
             return angle
         elif joint == 5:
-            angle = 32.0/25.0*cmd+75.0
+            angle = 32.0/25.0*(100-cmd)+75.0
             return angle
 
 
